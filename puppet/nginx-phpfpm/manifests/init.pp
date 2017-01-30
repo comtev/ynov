6b65bdda1c102d597d9764e7f020d906cac01bf5
Valentin COMTE <angenoir71880@gmail.com>
2017-01-30 19:12:17 +0100

    nginx default website

---
+++ puppet/nginx-phpfpm/manifests/init.pp
@@ -0,0 +1,64 @@
+class nginx {
+		package { 'nginx':
+				ensure => present,
+		}
+
+		service { 'nginx':
+				ensure     => running,
+				enable     => true,
+				hasrestart => true,
+				hasstatus  => true,
+				require    => Package['nginx'],
+		}
+
+		file { '/etc/nginx/sites-available/default':
+				ensure  => file,
+				mode    => '0644',
+				source  => 'puppet:///modules/nginx/default',
+				notify  => Service['nginx'],
+				require => Package['nginx'],
+		}
+
+		file { '/etc/nginx/sites-enabled/default':
+				ensure  => link,
+				target  => '/etc/nginx/sites-available/default',
+				require => File['/etc/nginx/sites-available/default'],
+				notify  => Service['nginx'],
+		}
+
+		package { 'php5-fpm':
+				ensure  => present,
+				require => Package['nginx'],
+		}
+
+		service { 'php5-fpm':
+				ensure     => running,
+				enable     => true,
+				hasrestart => true,
+				hasstatus  => true,
+				require    => Package['php5-fpm'],
+		}
+
+		file_line{"disable pathinfo":
+				path       => "/etc/php5/fpm/php.ini",
+				line       => 'cgi.fix_pathinfo=0',
+				require    => Package['php5-fpm'],
+				notify     => Service['php5-fpm'],
+		}
+
+		file_line{"make sure pathinfo is disabled":
+				path       => "/etc/php5/fpm/php.ini",
+				line       => 'cgi.fix_pathinfo=1',
+				ensure     => 'absent',
+				require    => Package['php5-fpm'],
+				notify     => Service['php5-fpm'],
+		}
+
+		file { '/var/www/html/index.php':
+				ensure  => file,
+				mode    => '0777',
+				source  => 'puppet:///modules/nginx/index.php',
+				require => Package['nginx'],
+		}
+
+}
